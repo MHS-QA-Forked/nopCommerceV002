df2fb87e559067cf6dd3edbf9bd2141dca1fc4a0
pavan <pavanoltraining@gmail.com>
2019-06-21 22:01:56 +0530

    This is my first commit.....

---
+++ nopCommerceV002/run.bat
@@ -0,0 +1,2 @@
+cd C:\Users\admin\eclipse-workspace\nopCommerceV002	
+mvn clean install
---
+++ nopCommerceV002/src/test/java/com/nopcommerce/testCases/TC_LoginTest_001.java
@@ -0,0 +1,47 @@
+package com.nopcommerce.testCases;
+
+import java.io.IOException;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.nopcommerce.pageObjects.LoginPage;
+import com.nopcommerce.testBase.BaseClass;
+
+
+public class TC_LoginTest_001 extends BaseClass {
+
+	@Test
+	public void loginTest() throws InterruptedException, IOException {
+		
+		driver.get(configPropObj.getProperty("baseURL"));
+		driver.manage().window().maximize(); 
+		
+		LoginPage lp = new LoginPage(driver);
+		
+		lp.setUserName(configPropObj.getProperty("useremail"));
+		logger.info("User provided"); //logger msg
+		
+		lp.setPassword(configPropObj.getProperty("password"));
+		logger.info("Password provided");//logger msg
+		
+		lp.clickLogin();
+		logger.info("Login in Clicked");//logger msg
+		
+		Thread.sleep(2000);
+		
+		if (driver.getTitle().equals("Dashboard / nopCommerce administration")) {
+			lp.clickLogout();
+			Assert.assertTrue(true);
+			logger.info("Login Passed");//logger msg
+			
+		} else {
+			captureScreen(driver,"loginTest");
+			logger.info("Login Failed");//logger msg
+			Assert.assertTrue(false);
+		}
+
+	}
+	
+	
+}
